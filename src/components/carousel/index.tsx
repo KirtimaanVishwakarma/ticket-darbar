608c68ea675f31155bad99be4c2df74d382b6e4c
MBMAC-511 <mbmac-511@Kirtimaan-Vishwakarma.local>
2024-08-26 18:28:13 +0530

    added carousel

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,31 +1,61 @@
 import React from 'react';
+import Slider from 'react-slick';
 
-const Carousel = () => {
+function Carousel() {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 1,
+    speed: 2000,
+    dots: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false
+  };
   return (
-    <div className='flex my-2 gap-2 overflow-auto'>
-      <img
-        src={
-          'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
-        }
-        alt='slide1'
-        className="rounded-lg"
-      />
-      <img
-        src={
-          'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
-        }
-        alt='slide1'
-        className="rounded-lg"
-      />
-      <img
-        src={
-          'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
-        }
-        alt='slide1'
-        className="rounded-lg"
-      />
+    <div className='slider-container'>
+      <Slider {...settings}>
+        <div className="px-2">
+          <img
+            src={
+              'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
+            }
+            alt='slide1'
+            className='rounded-lg m-auto w-full'
+          />
+        </div>
+        <div className="px-2">
+          <img
+            src={
+              'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
+            }
+            alt='slide1'
+          className='rounded-lg m-auto w-full'
+          />
+        </div>
+        <div className="px-2">
+          <img
+            src={
+              'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
+            }
+            alt='slide1'
+          className='rounded-lg m-auto w-full'
+          />
+        </div>
+        <div className="px-2">
+          <img
+            src={
+              'https://assets-in.bmscdn.com/promotions/cms/creatives/1722350498229_copyofwebshowcase1240x300.jpg'
+            }
+            alt='slide1'
+          className='rounded-lg m-auto w-full'
+          />
+        </div>
+      </Slider>
     </div>
   );
-};
+}
 
 export default Carousel;
