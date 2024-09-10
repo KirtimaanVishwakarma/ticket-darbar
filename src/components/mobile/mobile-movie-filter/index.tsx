c8b8d637301c5ec6172e8228816d5f21fa631978
MBMAC-511 <mbmac-511@Kirtimaan-Vishwakarma.local>
2024-09-10 16:04:05 +0530

    fixed filter animation

--- src/components/mobile/mobile-movie-filter/index.tsx
+++ src/components/mobile/mobile-movie-filter/index.tsx
@@ -4,36 +4,40 @@ import LocationPinIcon from '../../../../public/assets/location-pin.svg';
 import FilterIcon from '../../../../public/assets/filter-icon.svg';
 
 const MobileMovieFilter = () => {
-  // const [isScrolling, setIsScrolling] = useState(false);
-  // useEffect(() => {
-  //   let scrollTimeout: NodeJS.Timeout;
+  const [isScrolling, setIsScrolling] = useState(false);
+  useEffect(() => {
+    let scrollTimeout: NodeJS.Timeout;
 
-  //   const handleScroll = () => {
-  //     setIsScrolling(true);
+    const handleScroll = () => {
+      setIsScrolling(true);
+      console.log("test");
+      
 
-  //     if (scrollTimeout) {
-  //       clearTimeout(scrollTimeout);
-  //     }
+      if (scrollTimeout) {
+        clearTimeout(scrollTimeout);
+      }
 
-  //     scrollTimeout = setTimeout(() => {
-  //       setIsScrolling(false);
-  //     }, 150);
-  //   };
+      scrollTimeout = setTimeout(() => {
+        setIsScrolling(false);
+        console.log("test 1");
+        
+      }, 150);
+    };
 
-  //   window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //     clearTimeout(scrollTimeout);
-  //   };
-  // }, []);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(scrollTimeout);
+    };
+  }, []);
   return (
     <div
-      className={`sm:hidden sticky bottom-8 text-white flex justify-around`}
+      className={`sm:hidden fixed bottom-20 text-white flex w-full transition-all ease-in-out duration-400 justify-around ${isScrolling?'scale-0':'  scale-100'}`}
     >
       <div className='flex gap-2 text-sm bg-yellow-y1 p-3 items-center rounded-full'>
         <Image src={LocationPinIcon} height={22} alt='location' />
-        <header>Browse By Cinemas</header>
+        <header className="text-black text-xs">Browse By Cinemas</header>
       </div>
       <div className='flex text-sm bg-yellow-y1 p-3 items-center rounded-full'>
         <Image src={FilterIcon} height={22} alt='filter' />
